fbf7f69ad6abf92f0665dcfe58526f8a7b300002
Almaz <zarmond5@mail.ru>
2022-11-12 10:52:37 +0500

    my bot token

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-bot_token='5586515811:AAHvqZA7PQ2-B7-n2075cugAkaaS8K-ntmg'
+bot_token='5678859520:AAHLIKoT-6xsxoi8MAdB_LuG66UReNlFXZU'
 api_key_news='9c3ef6670db5418f9ea39fe82a21592d'
 category_list=['health', 'general', 'business', 'entertainment','sports', 'technology', 'science']
